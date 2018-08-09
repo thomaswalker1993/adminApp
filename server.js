da2ed95b34f7e35a3d6222816b88f1eed813d9b7
Tom <thomas_walker93@outlook.com>
2018-08-09 19:52:30 +0100

    addToHomescreen

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function(req, res) {
-  res.sendfile('public/index.html')
+  res.sendFile('public/index.html')
 })
 
 //Listen to port 3000
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './services/auth.service';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -36,7 +37,7 @@ const routes: Routes = [
     AppRoutingModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
